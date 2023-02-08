0c0c3567a97109290c97f7c912adc5c27c249dc9
Quentin Rousseau <contact@quent.in>
2023-02-08 07:28:40 -0800

    pgbouncerhero 2.0.0

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require turbolinks
-//= require pgbouncerhero/application
 //= require_tree .
